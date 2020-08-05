5dfce18723d917cc2edfafc5618fae87b31e798a
ForestCrazy <controt123@gmail.com>
2020-08-05 10:48:52 +0700

    dummy

--- index.php
+++ index.php
@@ -45,14 +45,14 @@
 <body>
     <!--Navbar -->
     <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
-        <a class="navbar-brand" href="#">Ecommerce</a>
+        <a class="navbar-brand" href="?page=home">Ecommerce</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#"><i class="far fa-home"></i> หน้าหลัก</a>
+                    <a class="nav-link" href="?page=home"><i class="far fa-home"></i> หน้าหลัก</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="far fa-store-alt"></i> ขายสินค้า</a>
@@ -61,7 +61,7 @@
                     <a class="nav-link" href="#"><i class="far fa-calendar-star"></i> กิจกรรม</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="far fa-gavel"></i> การประมูล</a>
+                    <a class="nav-link" href="?page=vote"><i class="far fa-gavel"></i> การประมูล</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
--- page/product.php
+++ page/product.php
@@ -1,21 +1,19 @@
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-4">
         <img class="card-img-top w-100" src="https://cf.shopee.co.th/file/7a81d50c5304f71ca9ff9f6824ca55ba" alt="Card image cap">
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-8">
         <p class="lead font-weight-bold">Product Name</p>
         <a><span class="badge purple mr-1">Category 2</span></a>
         <a><span class="badge blue mr-1">New</span></a>
         <a><span class="badge red mr-1">Bestseller</span></a>
-        <p class="price-product p-2" style="font-size: 2rem;">
-            <span class="mr-1">
-                <del>฿ 200</del>
-            </span>
-            <span>฿ 100</span>
-        </p>
+        <span class="mr-1">
+            <del>฿ 200</del>
+        </span>
+        <span>฿ 100</span>
         <div class="row">
-            <div class="col-4"><label class="shipping-label">การจัดส่ง</label></div>
-            <div class="col-8">
+            <div class="col-3"><label class="shipping-label">การจัดส่ง</label></div>
+            <div class="col-9">
                 <div class="shipping-type">
                     <!--<img src="https://image.flaticon.com/icons/svg/3176/3176178.svg" width="25px" height="25px">--><i class="fas fa-truck"></i> ฟรีค่าจัดส่ง</div>
             </div>
@@ -23,11 +21,11 @@
         <form name="add-product-to-cart" action="javascript:void(0)">
             <!-- Default input -->
             <div class="row">
-                <div class="col-4"><label class="shipping-label">จำนวน</label></div>
+                <div class="col-3"><label class="shipping-label">จำนวน</label></div>
                 <div class="col-3">
                     <input type="number" value="1" aria-label="Search" class="form-control w-100">
                 </div>
-                <div class="col-5">มีสินค้าทั้งหมด 500 ชิ้น</div>
+                <div class="col-6">มีสินค้าทั้งหมด 500 ชิ้น</div>
             </div>
             <br>
             <button class="btn btn-primary waves-effect waves-light">เพิ่มเข้าตะกร้า
@@ -38,7 +36,9 @@
     </div>
     <div class="col-sm-12">
         <div class="card">
-            ข้อมูลร้าน
+            <div class="card-body">
+                <img class="" src="">
+            </div>
         </div>
     </div>
     <div class="col-sm-12">
