63e93088d1a6d8c643d15a037e655b68253f8f73
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-07-29 23:51:26 +0700

    add star_score column

--- sql/product_review.sql
+++ sql/product_review.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `product_review` (
     `review_id` INT AUTO_INCREMENT,
     `sub_order_id` INT NOT NULL,
+    `star_score` INT NOT NULL,
     `review_msg` varchar(300),
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`review_id`)
