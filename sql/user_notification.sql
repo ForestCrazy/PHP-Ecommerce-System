7ca7f0bb1e9f30326d11baafc3394102207673d9
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-07-29 17:50:57 +0700

    add notification_url column

--- sql/user_notification.sql
+++ sql/user_notification.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `user_notification` (
     `notification_id` INT AUTO_INCREMENT,
     `u_id` INT NOT NULL,
     `notification_msg` varchar(300) NOT NULL,
+    `notification_url` varchar(100),
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`notification_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
