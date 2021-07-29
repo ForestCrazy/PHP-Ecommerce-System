8953121e991f8c04d2ca5251852d543b31dbdae1
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-07-29 15:03:29 +0700

    add store address

--- sql/store.sql
+++ sql/store.sql
@@ -5,5 +5,9 @@ CREATE TABLE IF NOT EXISTS `store` (
     `store_description` varchar(800) NOT NULL,
     `store_img` varchar(200),
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
+    `store_address` varchar(100) NOT NULL,
+    `store_city` varchar(100) NOT NULL,
+    `store_province` varchar(100) NOT NULL,
+    `store_zip_code` varchar(10) NOT NULL,
     PRIMARY KEY(`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
