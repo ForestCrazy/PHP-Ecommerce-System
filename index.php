7992519b9fde6a6594734680e46dc643515cb3f1
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-07-30 21:09:31 +0700

    add checkout page

--- index.php
+++ index.php
@@ -201,6 +201,8 @@ if (isset($_SESSION['username'])) {
             include_once __DIR__ . '/page/home.php';
         } elseif ($_GET['page'] == "product") {
             include_once __DIR__ . '/page/product.php';
+        } elseif ($_GET['page'] == "checkout") {
+            include_once __DIR__ . '/page/checkout.php';
         } elseif ($_GET['page'] == "event") {
             include_once __DIR__ . '/page/event.php';
         } elseif ($_GET['page'] == "cart") {
