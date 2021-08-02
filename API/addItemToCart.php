8bf3695a034c9dba5307154cf3a47a1e1b316b48
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-02 23:06:28 +0700

    check session username

--- API/addItemToCart.php
+++ API/addItemToCart.php
@@ -2,14 +2,14 @@
 require('../system/database.php');
 require('../system/oop.php');
 
-if (isset($_POST['submit_addToCart'])) {
+if (isset($_SESSION['username'])) {
     $sql_addToCart = 'INSERT INTO cart (u_id, product_id, quantity, shipping_id) VALUES ("' . $_SESSION['u_id'] . '", "' . mysqli_real_escape_string($connect, $_GET['p_id']) . '", "' . $_POST['quantity'] . '", "' . mysqli_real_escape_string($connect, $_POST['shipping_id']) . '")';
     $res_addToCart = mysqli_query($connect, $sql_addTOCart);
     if ($res_addToCart) {
         echo json_encode(array('success' => true));
     } else {
-        echo json_encode(array('success' => false));
+        echo json_encode(array('success' => false, 'code' => '500'));
     }
 } else {
-    echo json_encode(array('success' => false));
+    echo json_encode(array('success' => false, 'code' => '401'));
 }
