9d9a72cdda1a311c31c2d9493130f0f64c03ba31
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-03 12:18:32 +0700

    fix Undefined variable

--- API/addItemToCart.php
+++ API/addItemToCart.php
@@ -5,7 +5,7 @@ require('../system/oop.php');
 if (isset($_SESSION['username'])) {
     try {
         $sql_addToCart = 'INSERT INTO cart (u_id, product_id, quantity, shipping_id) VALUES ("' . $_SESSION['u_id'] . '", "' . mysqli_real_escape_string($connect, $_GET['p_id']) . '", "' . $_GET['quantity'] . '", "' . mysqli_real_escape_string($connect, $_GET['shipping_id']) . '")';
-        $res_addToCart = mysqli_query($connect, $sql_addTOCart);
+        $res_addToCart = mysqli_query($connect, $sql_addToCart);
         if ($res_addToCart) {
             echo json_encode(array('success' => true));
         } else {
