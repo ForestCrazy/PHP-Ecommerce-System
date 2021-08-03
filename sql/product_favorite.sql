ab22d12aec771c2b331880915598237d91d949f7
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-03 16:37:47 +0700

    add createtime column

--- sql/product_favorite.sql
+++ sql/product_favorite.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `product_favorite` (
     `u_id` INT NOT NULL,
     `product_id` INT NOT NULL,
+    `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`product_id`)
 ) ENGINE=InnoDB;
