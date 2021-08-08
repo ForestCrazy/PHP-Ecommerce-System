96362fc8be7a4176f3867df651e245d8915777c4
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-09 00:47:36 +0700

    API - lastShippingAddr

---
+++ API/lastShippingAddr.php
@@ -0,0 +1,16 @@
+<?php
+require('../system/database.php');
+require('../system/oop.php');
+
+if (isset($_SESSION['username'])) {
+    $sql_lastShippingAddr = 'SELECT * FROM `order` WHERE u_id = "' . $_SESSION['u_id'] . '" ORDER BY order_id DESC LIMIT 1';
+    $res_lastShippingAddr = mysqli_query($connect, $sql_lastShippingAddr);
+    if ($res_lastShippingAddr) {
+        $fetch_lastShippingAddr = mysqli_fetch_assoc($res_lastShippingAddr);
+        echo json_encode(array('success' => true, 'code' => 200, 'lastShippingAddr' => $fetch_lastShippingAddr['address_id']));
+    } else {
+        echo json_encode(array('success' => false, 'code' => 500));
+    }
+} else {
+    echo json_encode(array('success' => false, 'code' => 401));
+}
