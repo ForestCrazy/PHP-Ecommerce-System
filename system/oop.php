f14591c77521d96948266f270cb8e2667836c4e4
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-09 00:01:08 +0700

    add generateRandomString function

--- system/oop.php
+++ system/oop.php
@@ -23,3 +23,14 @@ function number_abbr($number)
 
     return $number;
 }
+
+function generateRandomString($length = 10)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
