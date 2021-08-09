1650164531728f2ab0c15095c33440541c222fc9
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-08-09 11:05:29 +0700

    API - IFNULL check

--- API/itemInCart.php
+++ API/itemInCart.php
@@ -3,7 +3,7 @@ require('../system/database.php');
 require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
-    $sql_cart = 'SELECT SUM(quantity) AS amountItemInCart FROM cart INNER JOIN product ON cart.product_id = product.product_id LEFT JOIN (SELECT product_id, img_url FROM product_img GROUP BY product_id ORDER BY weight ASC) AS product_img ON product.product_id = product_img.product_id INNER JOIN product_shipping ON product.product_id = product_shipping.product_id AND cart.shipping_id = product_shipping.shipping_provider_id WHERE cart.u_id = "' . $_SESSION['u_id'] . '"';
+    $sql_cart = 'SELECT IFNULL(SUM(quantity), 0) AS amountItemInCart FROM cart INNER JOIN product ON cart.product_id = product.product_id LEFT JOIN (SELECT product_id, img_url FROM product_img GROUP BY product_id ORDER BY weight ASC) AS product_img ON product.product_id = product_img.product_id INNER JOIN product_shipping ON product.product_id = product_shipping.product_id AND cart.shipping_id = product_shipping.shipping_provider_id WHERE cart.u_id = "' . $_SESSION['u_id'] . '"';
     $res_cart = mysqli_query($connect, $sql_cart);
     if ($res_cart) {
         // $res_array = array();
