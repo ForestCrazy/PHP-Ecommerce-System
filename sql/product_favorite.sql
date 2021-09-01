d1ca1e86208e9a2e8564cc0bf1f4c2467399ce33
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-02 03:07:05 +0700

    remove primary key

--- sql/product_favorite.sql
+++ sql/product_favorite.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `product_favorite` (
     `u_id` INT NOT NULL,
     `product_id` INT NOT NULL,
-    `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(`product_id`)
+    `createtime` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
