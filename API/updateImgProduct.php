5838fc2904beba8f33e5f7f2851f768a825bc9a6
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-02 14:15:18 +0700

    chnage product image file name after upload

--- API/updateImgProduct.php
+++ API/updateImgProduct.php
@@ -8,8 +8,8 @@ if (isset($_SESSION['username'])) {
             if (isset($_FILES['productImage'])) {
                 for ($i = 0; $i < count($_FILES['productImage']['name']); $i++) {
                     $timestamp = time();
-                    if (move_uploaded_file($_FILES['productImage']['tmp_name'][$i], '../asset/img/product_img/' . $timestamp . $_FILES['productImage']['name'][$i])) {
-                        $file_name = '/asset/img/product_img/' . $timestamp . $_FILES['productImage']['name'][$i];
+                    if (move_uploaded_file($_FILES['productImage']['tmp_name'][$i], '../asset/img/product_img/' . $timestamp . '-' . $_FILES['productImage']['name'][$i])) {
+                        $file_name = '/asset/img/product_img/' . $timestamp . '-' . $_FILES['productImage']['name'][$i];
                         $sql_ProductImage = 'INSERT INTO product_img (`product_id`, `weight`, `img_url`) VALUES ("' . $_POST['productId'] . '", "' . $i . '", "' . $file_name . '")';
                         $res_ProductImage = mysqli_query($connect, $sql_ProductImage);
                     }
