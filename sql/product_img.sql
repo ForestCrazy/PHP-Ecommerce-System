2ea23f34dda45c0cac430c686effbc7a328823d6
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-07 03:21:49 +0700

    Update product_img.sql

--- sql/product_img.sql
+++ sql/product_img.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS `product_img` (
     `product_id` INT NOT NULL,
     `weight` INT NOT NULL,
     `img_url` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
