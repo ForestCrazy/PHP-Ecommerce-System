91594a461f392247728c0f3502f11c69ec3303be
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-09 22:27:55 +0700

    change database table design

--- sql/address.sql
+++ sql/address.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS `address` (
     `address_id` INT AUTO_INCREMENT,
-    `address_token` varchar(20) NOT NULL,
-    `name` varchar(300),
+    `first_name` varchar(20) NOT NULL,
+    `last_name` varchar(300),
     `phone` varchar(20),
     `address` varchar(300),
     `city` varchar(20),
     `province` varchar(20),
     `zip_code` varchar(10),
     `u_id` INT NOT NULL,
+    `createtime` DATETIME default CURRENT_TIMESTAMP,
     PRIMARY KEY(`address_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
