5683cb39e6a5adf97dd29b5dcc2355a13d26ed0b
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-11 16:09:15 +0700

    change database table design

--- sql/order.sql
+++ sql/order.sql
@@ -2,7 +2,13 @@ CREATE TABLE IF NOT EXISTS `order` (
     `order_id` INT AUTO_INCREMENT,
     `shipping_provider_id` INT NOT NULL,
     `track_code` varchar(300),
-    `address_id` INT NOT NULL,
+    `first_name` varchar(20) NOT NULL,
+    `last_name` varchar(300),
+    `phone` varchar(20),
+    `address` varchar(300),
+    `city` varchar(20),
+    `province` varchar(20),
+    `zip_code` varchar(10),
     `u_id` INT NOT NULL,
     `status` ENUM('pending', 'processing', 'shipped', 'cancelled'),
     `payment_id` INT,
@@ -10,4 +16,4 @@ CREATE TABLE IF NOT EXISTS `order` (
     `context` varchar(300),
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
