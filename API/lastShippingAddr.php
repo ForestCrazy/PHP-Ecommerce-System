fe6598419c0fa16dd25fdd9efbf732c390771145
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-11 16:06:32 +0700

    handle if no recent order

--- API/lastShippingAddr.php
+++ API/lastShippingAddr.php
@@ -6,8 +6,12 @@ if (isset($_SESSION['username'])) {
     $sql_lastShippingAddr = 'SELECT * FROM `order` WHERE u_id = "' . $_SESSION['u_id'] . '" ORDER BY order_id DESC LIMIT 1';
     $res_lastShippingAddr = mysqli_query($connect, $sql_lastShippingAddr);
     if ($res_lastShippingAddr) {
-        $fetch_lastShippingAddr = mysqli_fetch_assoc($res_lastShippingAddr);
-        echo json_encode(array('success' => true, 'code' => 200, 'lastShippingAddr' => $fetch_lastShippingAddr['address_id']));
+        if (mysqli_num_rows($res_lastShippingAddr) == 1) {
+            $fetch_lastShippingAddr = mysqli_fetch_assoc($res_lastShippingAddr);
+            echo json_encode(array('success' => true, 'code' => 200, 'lastShippingAddr' => $fetch_lastShippingAddr['address_id']));
+        } else {
+            echo json_encode(array('success' => true, 'code' => 200, 'lastShippingAddr' => 0));
+        }
     } else {
         echo json_encode(array('success' => false, 'code' => 500));
     }
