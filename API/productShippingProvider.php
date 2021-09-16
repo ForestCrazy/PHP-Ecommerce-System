9ec10ce696efe392091d61c2dbd59ee41090c938
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-16 23:20:08 +0700

    API - productShippingProvider

--- API/productShippingProvider.php
+++ API/productShippingProvider.php
@@ -4,7 +4,7 @@ require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
     if (isset($_GET['p_id'])) {
-        $sql_productShipping = 'SELECT * FROM product_shipping WHERE product_id = "' . $_GET['p_id'] . '"';
+        $sql_productShipping = 'SELECT * FROM product_shipping INNER JOIN shipping_provider ON product_shipping.shipping_provider_id = shipping_provider.shipping_provider_id WHERE product_id = "' . $_GET['p_id'] . '"';
         $res_productShipping = mysqli_query($connect, $sql_productShipping);
         if ($res_productShipping) {
             echo json_encode(array('success' => true, 'code' => 200, 'data' => mysqli_fetch_all($res_productShipping, MYSQLI_ASSOC)));
