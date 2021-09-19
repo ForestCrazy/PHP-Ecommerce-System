6284c12ae70eec236cdf5605e58e279399baf782
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-19 16:05:29 +0700

    refactor code

--- system/oop.php
+++ system/oop.php
@@ -114,14 +114,7 @@ function createOrder($shipping_addr, $shipping_id, $payment_method, $shipping_pr
             $sql_order = 'INSERT INTO `order` (`shipping_provider_id`, `shipping_price`, `address_id`, `first_name`, `last_name`, `phone`, `address`, `city`, `province`, `zip_code`, `u_id`, `status`, `payment_method`) VALUES ("' . $shipping_id . '", "' . $shipping_price . '", "' . $fetch_shipping_addr['address_id'] . '", "' . $fetch_shipping_addr['first_name'] . '", "' . $fetch_shipping_addr['last_name'] . '", "' . $fetch_shipping_addr['phone'] . '", "' . $fetch_shipping_addr['address'] . '", "' . $fetch_shipping_addr['city'] . '", "' . $fetch_shipping_addr['province'] . '", "' . $fetch_shipping_addr['zip_code'] . '", "' . $_SESSION['u_id'] . '", "' . $order_status . '", "' . $payment_method . '")';
             $res_order = mysqli_query($connect, $sql_order);
             if ($res_order) {
-                $sql_order_id = 'SELECT order_id FROM `order` WHERE shipping_provider_id = "' . $shipping_id . '" AND u_id = "' . $_SESSION['u_id'] . '" ORDER BY order_id DESC LIMIT 1';
-                $res_order_id = mysqli_query($connect, $sql_order_id);
-                if ($res_order_id) {
-                    $fetch_order_id = mysqli_fetch_assoc($res_order_id);
-                    return json_encode(array('success' => true, 'code' => 200, 'orderId' => $fetch_order_id['order_id']));
-                } else {
-                    return json_encode(array('success' => false, 'code' => 500, 'reason' => 'ไม่สามารถหา orderId จากฐานข้อมูลได้'));
-                }
+                return json_encode(array('success' => true, 'code' => 200, 'orderId' => mysqli_insert_id($connect)));
             } else {
                 return json_encode(array('success' => false, 'code' => 500, 'reason' => 'สร้าง order ไม่สำเร็จ'));
             }
