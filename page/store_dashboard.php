065891e810ec5f4f4bee66e696e91b75d4267c01
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-23 07:11:26 +0700

    UI - store_dashboard

--- page/store_dashboard.php
+++ page/store_dashboard.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['username'])) {
         </script>
     <?php
     } else {
+        $sql_order = 'SELECT COUNT(order.order_id) AS order_amount FROM (SELECT order.order_id FROM `order` INNER JOIN sub_order ON order.order_id = sub_order.order_id INNER JOIN product ON sub_order.product_id = product.product_id WHERE product.store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '" AND order.status != "pending"';
     ?>
         <style>
             .chart {
@@ -24,7 +25,16 @@ if (!isset($_SESSION['username'])) {
                 <div class='card'>
                     <div class='card-body'>
                         <small>TODAY</small>
-                        <h3>10 Orders</h3>
+                        <?php
+                        $sql_order_today = $sql_order . ' AND DATE(order.createtime) = DATE(NOW()) GROUP BY order.order_id) AS `order`';
+                        $res_order_today = mysqli_query($connect, $sql_order_today);
+                        if ($res_order_today) {
+                            $fetch_order_today = mysqli_fetch_assoc($res_order_today);
+                        ?>
+                            <h3><?= $fetch_order_today['order_amount'] ?> Orders</h3>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
@@ -32,7 +42,16 @@ if (!isset($_SESSION['username'])) {
                 <div class='card'>
                     <div class='card-body'>
                         <small>MONTH</small>
-                        <h3>10 Orders</h3>
+                        <?php
+                        $sql_order_month = $sql_order . ' AND MONTH(order.createtime) = MONTH(NOW()) AND YEAR(order.createtime) = YEAR(NOW()) GROUP BY order.order_id) AS `order`';
+                        $res_order_month = mysqli_query($connect, $sql_order_month);
+                        if ($res_order_month) {
+                            $fetch_order_month = mysqli_fetch_assoc($res_order_month);
+                        ?>
+                            <h3><?= $fetch_order_month['order_amount'] ?> Orders</h3>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
@@ -40,14 +59,23 @@ if (!isset($_SESSION['username'])) {
                 <div class='card'>
                     <div class='card-body'>
                         <small>YEAR</small>
-                        <h3>10 Orders</h3>
+                        <?php
+                        $sql_order_year = $sql_order . ' AND YEAR(order.createtime) = YEAR(NOW()) GROUP BY order.order_id) AS `order`';
+                        $res_order_year = mysqli_query($connect, $sql_order_year);
+                        if ($res_order_year) {
+                            $fetch_order_year = mysqli_fetch_assoc($res_order_year);
+                        ?>
+                            <h3><?= $fetch_order_year['order_amount'] ?> Orders</h3>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
         <br />
         <div class='row'>
-            <div class='col-md-6'>
+            <!-- <div class='col-md-6'>
                 <div class='card'>
                     <div style="margin-top: 10px; padding-right: 15px;">
                         <div class="p-2 bd-highlight">Order History</div>
@@ -72,7 +100,7 @@ if (!isset($_SESSION['username'])) {
                         </script>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <div class='col-md-6'>
                 <div class='card'>
                     <div style="margin-top: 10px; padding-right: 15px;">
@@ -81,19 +109,36 @@ if (!isset($_SESSION['username'])) {
                     <div class='card-body'>
                         <div class='row'>
                             <canvas id="order_status" class='chart' width="400" height="400"></canvas>
+                            <?php
+                            $sql_order_success = 'SELECT COUNT(order.order_id) AS order_amount FROM (SELECT order.order_id FROM `order` INNER JOIN sub_order ON order.order_id = sub_order.order_id INNER JOIN product ON sub_order.product_id = product.product_id WHERE product.store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '" AND order.status = "success") AS `order`';
+                            $res_order_success = mysqli_query($connect, $sql_order_success);
+                            if ($res_order_success) {
+                                $fetch_order_success = mysqli_fetch_assoc($res_order_success);
+                            }
+                            $sql_order_processing = 'SELECT COUNT(order.order_id) AS order_amount FROM (SELECT order.order_id FROM `order` INNER JOIN sub_order ON order.order_id = sub_order.order_id INNER JOIN product ON sub_order.product_id = product.product_id WHERE product.store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '" AND order.status = "processing" AND track_code IS NULL) AS `order`';
+                            $res_order_processing = mysqli_query($connect, $sql_order_processing);
+                            if ($res_order_processing) {
+                                $fetch_order_processing = mysqli_fetch_assoc($res_order_processing);
+                            }
+                            $sql_order_shipping = 'SELECT COUNT(order.order_id) AS order_amount FROM (SELECT order.order_id FROM `order` INNER JOIN sub_order ON order.order_id = sub_order.order_id INNER JOIN product ON sub_order.product_id = product.product_id WHERE product.store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '" AND order.status = "processing" AND track_code IS NOT NULL) AS `order`';
+                            $res_order_shipping = mysqli_query($connect, $sql_order_shipping);
+                            if ($res_order_shipping) {
+                                $fetch_order_shipping = mysqli_fetch_assoc($res_order_shipping);
+                            }
+                            ?>
                             <script>
                                 var ctx = document.getElementById('order_status');
                                 var order_status = new Chart(ctx, {
                                     type: 'doughnut',
                                     data: {
                                         labels: [
-                                            'Red',
-                                            'Blue',
-                                            'Yellow'
+                                            'ออเดอร์ที่สำเร็จ',
+                                            'รอการจัดส่ง',
+                                            'อยู่ระหว่างการจัดส่ง'
                                         ],
                                         datasets: [{
                                             label: 'My First Dataset',
-                                            data: [300, 50, 100],
+                                            data: [<?= $fetch_order_success['order_amount'] ?>, <?= $fetch_order_processing['order_amount'] ?>, <?= $fetch_order_shipping['order_amount'] ?>],
                                             backgroundColor: [
                                                 'rgb(255, 99, 132)',
                                                 'rgb(54, 162, 235)',
@@ -104,7 +149,6 @@ if (!isset($_SESSION['username'])) {
                                     }
                                 });
                             </script>
-
                         </div>
                     </div>
                 </div>
