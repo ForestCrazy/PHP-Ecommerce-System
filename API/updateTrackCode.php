0c70aed17a2ee662c31e779e778de561a3d714aa
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-27 16:57:36 +0700

    API - updateTrackCode

---
+++ API/updateTrackCode.php
@@ -0,0 +1,34 @@
+<?php
+require('../system/database.php');
+require('../system/oop.php');
+
+if (isset($_SESSION['username'])) {
+    if (isset($_GET['order_id']) && isset($_GET['track_code'])) {
+        $sql_order = 'SELECT status FROM `order` WHERE order_id = "' . $_GET['order_id'] . '"';
+        $res_order = mysqli_query($connect, $sql_order);
+        if ($res_order) {
+            if (mysqli_num_rows($res_order) == 1) {
+                $fetch_order = mysqli_fetch_assoc($res_order);
+                if ($fetch_order['status'] == 'processing') {
+                    $sql_update_track_code = 'UPDATE `order` SET track_code = "' . $_GET['track_code'] . '" WHERE order_id = "' . $_GET['order_id'] . '"';
+                    $res_update_track_code = mysqli_query($connect, $sql_update_track_code);
+                    if ($res_update_track_code) {
+                        echo json_encode(array('success' => true, 'code' => 200));
+                    } else {
+                        echo json_encode(array('success' => false, 'code' => 500));
+                    }
+                } else {
+                    echo json_encode(array('success' => false, 'code' => 10100));
+                }
+            } else {
+                echo json_encode(array('success' => false ,'code' => 10102));
+            }
+        } else {
+            echo json_encode(array('success' => false, 'code' => 500));
+        }
+    } else {
+        echo json_encode(array('success' => false, 'code' => 10001));
+    }
+} else {
+    echo json_encode(array('success' => false, 'code' => 401));
+}
