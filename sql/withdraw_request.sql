18d3eae3a21e9cb0a532784562e53521ac2b9e13
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-27 17:07:40 +0700

    Create withdraw_request.sql

---
+++ sql/withdraw_request.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `withdraw_request` (
+    `withdraw_id` INT AUTO_INCREMENT,
+    `store_id` INT NOT NULL,
+    `withdraw_balance` INT NOT NULL,
+    `createtime` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `status` ENUM('pending', 'approve', 'decline') DEFAULT 'pending',
+    `update_status_time` DATETIME,
+    `withdraw_slip` varchar(80),
+    PRIMARY KEY(`withdraw_id`);
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
