16e9a884086fa90961f5438a3bf5733469a54b9e
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-29 13:00:17 +0700

    add store_balance when payment_method is tranfer

--- API/confirmSuccessOrder.php
+++ API/confirmSuccessOrder.php
@@ -4,7 +4,7 @@ require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
     if (isset($_POST['order_id'])) {
-        $sql_order = 'SELECT status FROM `order` WHERE order_id = "' . $_POST['order_id'] . '"';
+        $sql_order = 'SELECT status, payment_method, shipping_price + product_price AS total_price FROM `order` WHERE order_id = "' . $_POST['order_id'] . '"';
         $res_order = mysqli_query($connect, $sql_order);
         if ($res_order) {
             $fetch_order = mysqli_fetch_assoc($res_order);
@@ -12,7 +12,19 @@ if (isset($_SESSION['username'])) {
                 $sql_update_order = 'UPDATE `order` SET status = "shipped", end_process_time = NOW() WHERE order_id = "' . $_POST['order_id'] . '"';
                 $res_update_order = mysqli_query($connect, $sql_update_order);
                 if ($res_update_order) {
-                    echo json_encode(array('success' => true, 'code' => 200));
+                    if ($fetch_order['payment_method'] == 'tranfer') {
+                        $sql_update_store_balance = 'UPDATE store SET store_balance += "' . $fetch_order['total_price'] . '"';
+                        $res_update_store_balance = mysqli_query($connect, $sql_update_store_balance);
+                        if ($res_update_store_balance) {
+                            echo json_encode(array('success' => true, 'code' => 200));
+                        } else {
+                            $sql_update_order = 'UPDATE `order` SET status = "processing", end_process_time = NULL WHERE order_id = "' . $_POST['order_id'] . '"';
+                            $res_update_order = mysqli_query($connect, $sql_update_order);
+                            echo json_encode(array('success' => false, 'code' => 500));
+                        }
+                    } else {
+                        echo json_encode(array('success' => false, 'code' => 10100));
+                    }
                 } else {
                     echo json_encode(array('success' => false, 'code' => 500));
                 }
