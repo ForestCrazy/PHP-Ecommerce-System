f1a98871c051480e82b62b7ef910b31169ec7e8f
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-29 13:03:01 +0700

    add column

--- sql/withdraw_request.sql
+++ sql/withdraw_request.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS `withdraw_request` (
     `withdraw_id` INT AUTO_INCREMENT,
     `store_id` INT NOT NULL,
     `withdraw_balance` INT NOT NULL,
+    `fees` INT NOT NULL,
+    `bank_id` varchar(10) NOT NULL,
     `createtime` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `status` ENUM('pending', 'approve', 'decline') DEFAULT 'pending',
     `update_status_time` DATETIME,
     `withdraw_slip` varchar(80),
-    PRIMARY KEY(`withdraw_id`);
+    PRIMARY KEY(`withdraw_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
