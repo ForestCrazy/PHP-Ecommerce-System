35038d89779ab36aff3a515a67a991857742c200
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-30 14:42:51 +0700

    add order_id column

--- sql/payment.sql
+++ sql/payment.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS `payment` (
     `payment_id` INT AUTO_INCREMENT,
     `u_id` INT NOT NULL,
     `pay_slip` varchar(100) NOT NULL,
+    `order_id` INT NOT NULL,
     `status` ENUM('pending', 'approve', 'decline') default 'pending',
     `update_status_time` datetime,
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`payment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
