daab743d2c42105aaf525b6a1530f976ec046b2a
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-30 13:53:30 +0700

    UI - manage_account

---
+++ page/admin/manage_account.php
@@ -0,0 +1,37 @@
+<?php
+if (!isset($_SESSION['username'])) {
+    gotoPage('home');
+} else {
+    $sql_user = 'SELECT * FROM user';
+    $res_user = mysqli_query($connect, $sql_user);
+    if ($res_user) {
+?>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Createtime</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($fetch_user = mysqli_fetch_assoc($res_user)) {
+                    ?>
+                        <tr>
+                            <th scope="row"><?= $fetch_user['u_id'] ?></th>
+                            <td><?= $fetch_user['username'] ?></td>
+                            <td><?= $fetch_user['email'] ?></td>
+                            <td><?= $fetch_user['createtime'] ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+<?php
+    }
+}
