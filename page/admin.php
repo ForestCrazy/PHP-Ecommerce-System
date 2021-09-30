d4725509bcdee92d21b79675aaaaf1964ad88974
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-30 13:53:11 +0700

    include admin page

---
+++ page/admin.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_SESSION['username'])) {
+    gotoPage('home');
+} else {
+    if (isAdmin($_SESSION['u_id'])) {
+        if (!isset($_GET['admin'])) {
+            $_GET['admin'] = 'manage_account';
+        }
+        if ($_GET['admin'] == 'manage_account') {
+            include_once __DIR__ . './admin/manage_account.php';
+        } elseif ($_GET['admin'] == 'manage_order') {
+            include __DIR__ . './admin/manage_order.php';
+        } elseif ($_GET['admin'] == 'manage_withdraw') {
+            include __DIR__ . './admin/manage_withdraw.php';
+        } elseif ($_GET['admin'] == 'approve_payment') {
+            include __DIR__ . './admin/approve_payment.php';
+        } elseif ($_GET['admin'] == 'manage_banner') {
+            include __DIR__ . './admin/manage_banner.php';
+        } else {
+            gotoPage('admin');
+        }
+    } else {
+        gotoPage('home');
+    }
+}
