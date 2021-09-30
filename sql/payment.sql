985f5473aa4f73bcf7d8b565d6f96ec24097af94
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-09-30 14:30:15 +0700

    change column name

--- sql/payment.sql
+++ sql/payment.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `payment` (
     `payment_id` INT AUTO_INCREMENT,
     `u_id` INT NOT NULL,
     `pay_slip` varchar(100) NOT NULL,
-    `approve` boolean DEFAULT false,
-    `approve_time` datetime,
+    `status` ENUM('pending', 'approve', 'decline') default 'pending',
+    `update_status_time` datetime,
     `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`payment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
