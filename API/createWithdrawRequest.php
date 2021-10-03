130595473ee2d7238f6e79bd645f198fe2c53148
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-10-03 13:15:00 +0700

    check hasOwnStore

--- API/createWithdrawRequest.php
+++ API/createWithdrawRequest.php
@@ -3,51 +3,55 @@ require('../system/database.php');
 require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
-    if (isset($_POST['withdraw_amount']) && isset($_POST['account_number']) && isset($_POST['bank_id'])) {
-        if (!empty($_POST['account_number']) && !empty($_POST['bank_id'])) {
-            $sql_bank_withdraw = 'SELECT bank_id FROM bank_withdraw WHERE bank_id = "' . $_POST['bank_id'] . '"';
-            $res_bank_withdraw = mysqli_query($connect, $sql_bank_withdraw);
-            if ($res_bank_withdraw) {
-                if (mysqli_num_rows($res_bank_withdraw) == 1) {
-                $sql_store_balance = 'SELECT store_balance FROM store WHERE store_id = "' . hasOwnStore($_SESSION['u_id']) . '"';
-                $res_store_balance = mysqli_query($connect, $sql_store_balance);
-                if ($res_store_balance) {
-                    $fetch_store_balance = mysqli_fetch_assoc($res_store_balance);
-                    if ($fetch_store_balance['store_balance'] >= $_POST['withdraw_amount']) {
-                        $sql_update_store = 'UPDATE store SET store_balance = store_balance - ' . $_POST['withdraw_amount'] . ' WHERE store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '"';
-                        $res_update_store = mysqli_query($connect, $sql_update_store);
-                        if ($res_update_store) {
-                            $fees = intval($_POST['withdraw_amount'] / 100 * 3);
-                            $withdraw_balance = $_POST['withdraw_amount'] - $fees;
-                            $sql_withdraw_request = 'INSERT INTO withdraw_request (store_id, withdraw_balance, fees, bank_id, account_number) VALUES ("' . hasOwnStore($_SESSION['u_id'], true) . '", "' . $withdraw_balance . '", "' . $fees . '", "' . $_POST['bank_id'] . '", "' . $_POST['account_number'] . '")';
-                            $res_withdraw_request = mysqli_query($connect, $sql_withdraw_request);
-                            if ($res_withdraw_request) {
-                                echo json_encode(array('success' => true, 'code' => 200));
-                            } else {
-                                $sql_update_store = 'UPDATE store SET store_balance = store_balance + ' . $_POST['withdraw_amount'] . ' WHERE store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '"';
+    if (hasOwnStore($_SESSION['u_id'])) {
+        if (isset($_POST['withdraw_amount']) && isset($_POST['account_number']) && isset($_POST['bank_id'])) {
+            if (!empty($_POST['account_number']) && !empty($_POST['bank_id'])) {
+                $sql_bank_withdraw = 'SELECT bank_id FROM bank_withdraw WHERE bank_id = "' . $_POST['bank_id'] . '"';
+                $res_bank_withdraw = mysqli_query($connect, $sql_bank_withdraw);
+                if ($res_bank_withdraw) {
+                    if (mysqli_num_rows($res_bank_withdraw) == 1) {
+                        $sql_store_balance = 'SELECT store_balance FROM store WHERE store_id = "' . hasOwnStore($_SESSION['u_id']) . '"';
+                        $res_store_balance = mysqli_query($connect, $sql_store_balance);
+                        if ($res_store_balance) {
+                            $fetch_store_balance = mysqli_fetch_assoc($res_store_balance);
+                            if ($fetch_store_balance['store_balance'] >= $_POST['withdraw_amount']) {
+                                $sql_update_store = 'UPDATE store SET store_balance = store_balance - ' . $_POST['withdraw_amount'] . ' WHERE store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '"';
                                 $res_update_store = mysqli_query($connect, $sql_update_store);
-                                echo json_encode(array('success' => false, 'code' => 500));
+                                if ($res_update_store) {
+                                    $fees = intval($_POST['withdraw_amount'] / 100 * 3);
+                                    $withdraw_balance = $_POST['withdraw_amount'] - $fees;
+                                    $sql_withdraw_request = 'INSERT INTO withdraw_request (store_id, withdraw_balance, fees, bank_id, account_number) VALUES ("' . hasOwnStore($_SESSION['u_id'], true) . '", "' . $withdraw_balance . '", "' . $fees . '", "' . $_POST['bank_id'] . '", "' . $_POST['account_number'] . '")';
+                                    $res_withdraw_request = mysqli_query($connect, $sql_withdraw_request);
+                                    if ($res_withdraw_request) {
+                                        echo json_encode(array('success' => true, 'code' => 200));
+                                    } else {
+                                        $sql_update_store = 'UPDATE store SET store_balance = store_balance + ' . $_POST['withdraw_amount'] . ' WHERE store_id = "' . hasOwnStore($_SESSION['u_id'], true) . '"';
+                                        $res_update_store = mysqli_query($connect, $sql_update_store);
+                                        echo json_encode(array('success' => false, 'code' => 500));
+                                    }
+                                } else {
+                                    echo json_encode(array('success' => false, 'code' => 500));
+                                }
+                            } else {
+                                echo json_encode(array('success' => false, 'code' => 10100, 'reason' => 'ยอดเงินคงเหลือของร้านไม่เพียงพอสำหรับจำนวนที่จะถอน'));
                             }
                         } else {
                             echo json_encode(array('success' => false, 'code' => 500));
                         }
                     } else {
-                        echo json_encode(array('success' => false, 'code' => 10100, 'reason' => 'ยอดเงินคงเหลือของร้านไม่เพียงพอสำหรับจำนวนที่จะถอน'));
+                        echo json_encode(array('success' => false, 'code' => 10102));
                     }
                 } else {
                     echo json_encode(array('success' => false, 'code' => 500));
                 }
             } else {
-                echo json_encode(array('success' => false, 'code' => 10102));
-            }
-            } else {
-                echo json_encode(array('success' => false, 'code' => 500));
+                echo json_encode(array('success' => false, 'code' => 10003));
             }
         } else {
-            echo json_encode(array('success' => false, 'code' => 10003));
+            echo json_encode(array('success' => false, 'code' => 10001));
         }
     } else {
-        echo json_encode(array('success' => false, 'code' => 10001));
+        echo json_encode(array('success' => false, 'code' => 403));
     }
 } else {
     echo json_encode(array('success' => false, 'code' => 401));
--- API/updateTrackCode.php
+++ API/updateTrackCode.php
@@ -3,31 +3,35 @@ require('../system/database.php');
 require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
-    if (isset($_GET['order_id']) && isset($_GET['track_code'])) {
-        $sql_order = 'SELECT status FROM `order` WHERE order_id = "' . $_GET['order_id'] . '"';
-        $res_order = mysqli_query($connect, $sql_order);
-        if ($res_order) {
-            if (mysqli_num_rows($res_order) == 1) {
-                $fetch_order = mysqli_fetch_assoc($res_order);
-                if ($fetch_order['status'] == 'processing') {
-                    $sql_update_track_code = 'UPDATE `order` SET track_code = "' . $_GET['track_code'] . '" WHERE order_id = "' . $_GET['order_id'] . '"';
-                    $res_update_track_code = mysqli_query($connect, $sql_update_track_code);
-                    if ($res_update_track_code) {
-                        echo json_encode(array('success' => true, 'code' => 200));
+    if (hasOwnStore($_SESSION['u_id'])) {
+        if (isset($_GET['order_id']) && isset($_GET['track_code'])) {
+            $sql_order = 'SELECT status FROM `order` WHERE order_id = "' . $_GET['order_id'] . '"';
+            $res_order = mysqli_query($connect, $sql_order);
+            if ($res_order) {
+                if (mysqli_num_rows($res_order) == 1) {
+                    $fetch_order = mysqli_fetch_assoc($res_order);
+                    if ($fetch_order['status'] == 'processing') {
+                        $sql_update_track_code = 'UPDATE `order` SET track_code = "' . $_GET['track_code'] . '" WHERE order_id = "' . $_GET['order_id'] . '"';
+                        $res_update_track_code = mysqli_query($connect, $sql_update_track_code);
+                        if ($res_update_track_code) {
+                            echo json_encode(array('success' => true, 'code' => 200));
+                        } else {
+                            echo json_encode(array('success' => false, 'code' => 500));
+                        }
                     } else {
-                        echo json_encode(array('success' => false, 'code' => 500));
+                        echo json_encode(array('success' => false, 'code' => 10100));
                     }
                 } else {
-                    echo json_encode(array('success' => false, 'code' => 10100));
+                    echo json_encode(array('success' => false, 'code' => 10102));
                 }
             } else {
-                echo json_encode(array('success' => false ,'code' => 10102));
+                echo json_encode(array('success' => false, 'code' => 500));
             }
         } else {
-            echo json_encode(array('success' => false, 'code' => 500));
+            echo json_encode(array('success' => false, 'code' => 10001));
         }
     } else {
-        echo json_encode(array('success' => false, 'code' => 10001));
+        echo json_encode(array('success' => false, 'code' => 403));
     }
 } else {
     echo json_encode(array('success' => false, 'code' => 401));
