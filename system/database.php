155db30e9e7a914b204bdaaa803c14eb3231f687
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-10-04 00:04:29 +0700

    change database username

--- system/database.php
+++ system/database.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('localhost', 'root', '', 'ecommerce');
+$connect = mysqli_connect('localhost', 'forestcrazy', 'n7L#tVzn]YsZHud1', 'ecommerce');
 
 session_start();
 date_default_timezone_set("Asia/Bangkok");
