0ae0e41a5c85ae425abf4c25d93871d29d0c9f86
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-10-03 13:15:18 +0700

    check isAdmin

--- API/updatePaymentSlipStatus.php
+++ API/updatePaymentSlipStatus.php
@@ -3,43 +3,47 @@ require('../system/database.php');
 require('../system/oop.php');
 
 if (isset($_SESSION['username'])) {
-    if (isset($_POST['payment_id']) && isset($_POST['status'])) {
-        $sql_payment = 'SELECT status, order_id FROM payment WHERE payment_id = "' . $_POST['payment_id'] . '"';
-        $res_payment = mysqli_query($connect, $sql_payment);
-        if ($res_payment) {
-            if (mysqli_num_rows($res_payment) == 1) {
-                $fetch_payment = mysqli_fetch_assoc($res_payment);
-                if ($fetch_payment['status'] == 'pending') {
-                    $sql_update_payment = 'UPDATE payment SET status = "' . $_POST['status'] . '", update_status_time = NOW() WHERE payment_id = "' . $_POST['payment_id'] . '"';
-                    $res_update_payment = mysqli_query($connect, $sql_update_payment);
-                    if ($res_update_payment) {
-                        if ($_POST['status'] == 'approve') {
-                            $sql_update_order = 'UPDATE `order` SET status = "processing" WHERE order_id = "' . $fetch_payment['order_id'] . '"';
-                            $res_update_order = mysqli_query($connect, $sql_update_order);
-                            if ($res_update_order) {
-                                echo json_encode(array('success' => true, 'code' => 200));
+    if (isAdmin($_SESSION['u_id'])) {
+        if (isset($_POST['payment_id']) && isset($_POST['status'])) {
+            $sql_payment = 'SELECT status, order_id FROM payment WHERE payment_id = "' . $_POST['payment_id'] . '"';
+            $res_payment = mysqli_query($connect, $sql_payment);
+            if ($res_payment) {
+                if (mysqli_num_rows($res_payment) == 1) {
+                    $fetch_payment = mysqli_fetch_assoc($res_payment);
+                    if ($fetch_payment['status'] == 'pending') {
+                        $sql_update_payment = 'UPDATE payment SET status = "' . $_POST['status'] . '", update_status_time = NOW() WHERE payment_id = "' . $_POST['payment_id'] . '"';
+                        $res_update_payment = mysqli_query($connect, $sql_update_payment);
+                        if ($res_update_payment) {
+                            if ($_POST['status'] == 'approve') {
+                                $sql_update_order = 'UPDATE `order` SET status = "processing" WHERE order_id = "' . $fetch_payment['order_id'] . '"';
+                                $res_update_order = mysqli_query($connect, $sql_update_order);
+                                if ($res_update_order) {
+                                    echo json_encode(array('success' => true, 'code' => 200));
+                                } else {
+                                    $sql_update_payment = 'UPDATE payment SET status = "pending", update_status_time = NULL WHERE payment_id = "' . $_POST['payment_id'] . '"';
+                                    $res_update_payment = mysqli_query($connect, $sql_update_payment);
+                                    echo json_encode(array('success' => false, 'code' => 500));
+                                }
                             } else {
-                                $sql_update_payment = 'UPDATE payment SET status = "pending", update_status_time = NULL WHERE payment_id = "' . $_POST['payment_id'] . '"';
-                                $res_update_payment = mysqli_query($connect, $sql_update_payment);
-                                echo json_encode(array('success' => false, 'code' => 500));
+                                echo json_encode(array('success' => true, 'code' => 200));
                             }
                         } else {
-                            echo json_encode(array('success' => true, 'code' => 200));
+                            echo json_encode(array('success' => false, 'code' => 500));
                         }
                     } else {
-                        echo json_encode(array('success' => false, 'code' => 500));
+                        echo json_encode(array('success' => false, 'code' => 10100, 'reason' => 'การชำระเงินไม่ได้อยู่ในสถานะรออนุมัติ'));
                     }
                 } else {
-                    echo json_encode(array('success' => false, 'code' => 10100, 'reason' => 'การชำระเงินไม่ได้อยู่ในสถานะรออนุมัติ'));
+                    echo json_encode(array('success' => false, 'code' => 10102));
                 }
             } else {
-                echo json_encode(array('success' => false, 'code' => 10102));
+                echo json_encode(array('success' => false, 'code' => 500));
             }
         } else {
-            echo json_encode(array('success' => false, 'code' => 500));
+            echo json_encode(array('success' => false, 'code' => 10001));
         }
     } else {
-        echo json_encode(array('success' => false, 'code' => 10001));
+        echo json_encode(array('success' => false, 'code' => 403));
     }
 } else {
     echo json_encode(array('success' => false, 'code' => 401));
